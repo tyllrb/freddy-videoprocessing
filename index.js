28ad2e5739c6d23f4a5129aa0c072dd805dbfc6d
Tyler <tyler.34@hotmail.com>
2019-10-10 17:00:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded());
 app.use('/video', video);
 
 // https://cloud.mongodb.com/v2/5d9f8d52553855931f1309b7#clusters/detail/sandbox/connect?clusterId=sandbox
-// mongoose.connect(`mongodb+srv://tyler:x9tvlpw@sandbox-5b8ag.mongodb.net/test?retryWrites=true&w=majority`);
+// mongoose.connect(`mongodb+srv://tyler:password1@sandbox-5b8ag.mongodb.net/test?retryWrites=true&w=majority`);
 // const db = mongoose.connection;
 
 console.log(db ? 'established connection to database' : 'could not establish connection to database');
